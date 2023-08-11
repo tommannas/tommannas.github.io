c2be0b62f781b8725bb610d31cc7d8461f8b5417
Tom Mannas <140963830+tommannas@users.noreply.github.com>
2023-08-11 13:19:00 +0100

    Update [SQL] Covid Portfolio Project Script.sql

--- [SQL] Covid Portfolio Project Script.sql
+++ [SQL] Covid Portfolio Project Script.sql
@@ -1,3 +1,10 @@
+/*
+Covid-19 Dataset Exploration & Insights
+
+Skills Used: Joins, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+
+*/
+
 SELECT *
 FROM PortfolioProject..CovidDeaths
 WHERE continent is not null
